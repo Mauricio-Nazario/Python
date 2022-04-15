2d37ad630e8533fbc895345435c6839a71bcfc7f
Mauricio-Nazario <69878847+Mauricio-Nazario@users.noreply.github.com>
2022-04-15 17:20:11 -0300

    Add files via upload

---
+++ Desafio002/Desafio002.py
@@ -0,0 +1,5 @@
+dia = input('Informe do dia de nascimento: ')
+mes = input('Informe o mês de nascimento: ')
+ano = input('Informe o ano de nascimento: ')
+print('Você nasceu no dia', dia, 'de', mes, 'de', ano, '. Correto?')
+

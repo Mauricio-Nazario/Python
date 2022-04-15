83e5ec684cc544875d12e6511da4b26cae36bdc3
Mauricio-Nazario <69878847+Mauricio-Nazario@users.noreply.github.com>
2022-04-15 17:26:49 -0300

    Add files via upload

---
+++ Desafio011/Desafio011.py
@@ -0,0 +1,7 @@
+a = float(input('informe a altura da parede: '))
+l = float(input('Informe a largura da parede: '))
+
+area = a * l
+qnt = area / 2
+
+print('A área da parede é {}  e a quantidade de tinta é {}'.format(area, qnt))
